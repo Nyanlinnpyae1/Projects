524fa7e6a85da5c6b4ca122ba15a90898c677442
Nyanlinnpyae <falcongerxx@gmail.com>
2022-05-03 11:58:45 +0630

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let weather={
+    "apiKey": "9a14a857a3cc305366b19c513d331289",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + apiKey 
+        ).then((response) => response.json())
+        .then((data)=>this.console.log());
+    },
+    displayWeather: function(data){
+     const { name }=data;
+     const { icon, description }=data.weather[0];
+     const { temp,humidity }=data.main;
+     const { speed }=data.wind;    
+     console.log(name,icon,description,temp,humidity,speed);
+     document.querySelector(".city").innerText = "Weather in " + name;
+     document.querySelector(".icon").src = "https://openweathermap.org/img/wn" + ".png";
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity" + humidity + "%";
+    document.querySelector(".wind").ineerText = "Wind Speed" + speed + "km/h";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search button").addEventListener("click",function(){
+    weather.search();
+});
